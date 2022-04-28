bedf25b97e1f4e693eeede9d5c8a7d039e9fef76
Varga Zsolt <hello@hisorange.me>
2022-04-28 03:50:56 +0200

    Refactor the alter code to be more modular

--- src/interface/column.interface.ts
+++ src/interface/column.interface.ts
@@ -1,8 +1,24 @@
 import { ColumnType } from '../enum/column-type.enum';
 
 export interface IColumn {
+  /**
+   * Columns can be defined as a traditional column,
+   * but also as virtual / stored / computed columns.
+   */
   kind: 'column';
+
+  /**
+   * Mapping to the PostgreSQL 14's data type
+   */
   type: ColumnType;
+
+  /**
+   * Nullable rule.
+   */
   isNullable: boolean;
+
+  /**
+   * Unique contraint.
+   */
   isUnique: boolean;
 }
--- src/migrator/alter.table.ts
+++ src/migrator/alter.table.ts
@@ -1,6 +1,7 @@
 import { diff } from 'just-diff';
 import { Knex } from 'knex';
 import { ISchema } from '../interface/schema.interface';
+import { alterNullable, alterUnique } from './alter.column';
 import { createColumn } from './create.column';
 
 export const alterTable = (
@@ -8,44 +9,39 @@ export const alterTable = (
   currentSchema: ISchema,
   expectedSchema: ISchema,
 ): Knex.SchemaBuilder => {
-  return builder.alterTable(expectedSchema.name, async alter => {
-    const differences = diff(currentSchema, expectedSchema);
+  return builder.alterTable(expectedSchema.name, async builder => {
+    const difference = diff(currentSchema, expectedSchema);
 
-    // console.log('Differences:', differences);
-    // console.log('Current schema:', currentSchema);
-    // console.log('Expected schema:', expectedSchema);
-
-    for (const change of differences) {
+    for (const change of difference) {
       const { op, path } = change;
 
       if (path[0] === 'columns') {
-        const columnName = path[1] as string;
+        const name = path[1] as string;
 
         switch (op) {
           // New column added
           case 'add':
-            createColumn(alter, columnName, expectedSchema.columns[columnName]);
+            createColumn(builder, name, expectedSchema.columns[name]);
             break;
 
           // Column removed
           case 'remove':
-            alter.dropColumn(columnName);
+            builder.dropColumn(name);
             break;
 
-          // Column changed
+          // Column altered
           case 'replace':
-            // Change the nullable state
-            if (path[1] === columnName && path[2] === 'isNullable') {
-              if (expectedSchema.columns[columnName].isNullable) {
-                alter.setNullable(columnName);
-              } else {
-                alter.dropNullable(columnName);
-              }
-            } else if (path[1] === columnName && path[2] === 'isUnique') {
-              if (expectedSchema.columns[columnName].isUnique) {
-                alter.unique([columnName]);
-              } else {
-                alter.dropUnique([columnName]);
+            if (path[1] === name) {
+              const def = expectedSchema.columns[name];
+
+              // Route the alteration based on the change
+              switch (path[2]) {
+                case 'isNullable':
+                  alterNullable(builder, name, def.isNullable);
+                  break;
+                case 'isUnique':
+                  alterUnique(builder, name, def.isUnique);
+                  break;
               }
             } else {
               console.error(
@@ -54,16 +50,6 @@ export const alterTable = (
               );
             }
         }
-      } else {
-        console.error(
-          'Current',
-          currentSchema,
-          'Expected',
-          expectedSchema,
-          'Change',
-          change,
-        );
-        throw new Error('Logged change is not supported');
       }
     }
   });
