f0d4f03059c0d2e57a045932c60dbbe461c879de
Zsolt Varga <hello@hisorange.me>
2023-03-28 22:21:39 +0200

    Dedicated model manager component

--- src/inductor.ts
+++ src/inductor.ts
@@ -1,20 +1,12 @@
-import { Model, ModelClass, Pojo } from 'objection';
-import { ModelNotFound } from './exception/model-not-found.exception';
+import { Model, ModelClass } from 'objection';
 import { Migrator } from './library/migrator';
+import { ModelManager } from './library/model.manager';
 import { Plan } from './library/plan';
-import { ValidateTable } from './library/table.validator';
 import { IDatabase } from './types/database.interface';
 import { IStepResult } from './types/step-result.interface';
 import { ITable } from './types/table.interface';
-import { TableMap } from './types/table.map';
-import { ColumnTools } from './utils/column-tools';
 
 export class Inductor {
-  /**
-   * Map table descriptions with their model classes.
-   */
-  protected tableMap: TableMap = new Map();
-
   /**
    * Session ID for this instance, allows to track the logs, and queries.
    */
@@ -24,83 +16,13 @@ export class Inductor {
    * Migrator is responsible for applying the changes to the database.
    */
   readonly migrator: Migrator;
+  readonly models: ModelManager;
 
   constructor(readonly database: IDatabase) {
     this.migrator = new Migrator(this.id, database);
 
-    this.updateTableMap(database.tables);
-  }
-
-  /**
-   * Convert the table into a model class.
-   */
-  protected toModel(table: ITable): ModelClass<Model> {
-    ValidateTable(table);
-
-    // Prepare fast lookup maps for both propery and column name conversions.
-    // Even tho this is a small amount of data, it's worth it to avoid
-    // having to iterate over the properties of the model class.
-    const columnMap = new Map<string, string>();
-    const propertyMap = new Map<string, string>();
-
-    for (const columnName in table.columns) {
-      if (Object.prototype.hasOwnProperty.call(table.columns, columnName)) {
-        // TODO map from meta defintion
-
-        // Map column names to property names
-        columnMap.set(columnName, columnName);
-        // Map property names to column names
-        propertyMap.set(columnName, columnName);
-      }
-    }
-
-    // Map database columns to code level references
-    // Database -> Model = Getter
-    const databaseToModel = (dbPojo: Pojo): Pojo => {
-      const modelPojo: Pojo = {};
-
-      for (const columnName of Object.keys(dbPojo)) {
-        modelPojo[columnMap.get(columnName)!] = dbPojo[columnName];
-      }
-
-      return dbPojo;
-    };
-
-    // Map code level references to database columns
-    // Database -> Model = Setter
-    const modelToDatabase = (modelPojo: Pojo): Pojo => {
-      const dbPojo: Pojo = {};
-
-      for (const propertyName of Object.keys(modelPojo)) {
-        dbPojo[propertyMap.get(propertyName)!] = modelPojo[propertyName];
-      }
-
-      return dbPojo;
-    };
-
-    // Hook before the model is created
-    const onCreate = () => {};
-
-    // Hook before the model is updated
-    const onUpdate = () => {};
-
-    const model = class extends Model {};
-
-    model.tableName = table.name;
-    model.idColumn = ColumnTools.filterPrimary(table);
-
-    model.columnNameMappers = {
-      parse: databaseToModel,
-      format: modelToDatabase,
-    };
-
-    // model.prototype.$beforeInsert = onCreate;
-    // model.prototype.$beforeUpdate = onUpdate;
-
-    // Associate the knex instance with the newly created model class.
-    model.knex(this.migrator.knex);
-
-    return model;
+    this.models = new ModelManager(this.migrator);
+    this.models.setTables(database.tables);
   }
 
   compareState(tables: ITable[]): Promise<Plan> {
@@ -111,43 +33,21 @@ export class Inductor {
     return this.compareState(tables)
       .then(plan => plan.execute())
       .then(result => {
-        this.updateTableMap(tables);
+        this.models.setTables(tables);
 
         return result;
       });
   }
 
-  protected updateTableMap(tables: ITable[]) {
-    tables.forEach(b => {
-      for (const columnName in b.columns) {
-        b.columns[columnName].capabilities.sort((a, b) => a - b);
-      }
-    });
-
-    this.tableMap = new Map(
-      tables.map(table => [
-        table.name,
-        {
-          table,
-          model: this.toModel(table),
-        },
-      ]),
-    );
-  }
-
   async readState(filters: string[] = []): Promise<ITable[]> {
     return this.migrator.readDatabaseState(filters).then(state => {
-      this.updateTableMap(state);
+      this.models.setTables(state);
       return state;
     });
   }
 
   getModel<T extends Model = Model>(table: string): ModelClass<T> {
-    if (!this.tableMap.has(table)) {
-      throw new ModelNotFound(table);
-    }
-
-    return this.tableMap.get(table)!.model as ModelClass<T>;
+    return this.models.getModel<T>(table);
   }
 
   closeConnection() {
@@ -155,10 +55,14 @@ export class Inductor {
   }
 
   getTableDescriptors(): ITable[] {
-    return Array.from(this.tableMap.values()).map(({ table: table }) => table);
+    return Array.from(this.models.getTableMap().values()).map(
+      ({ table }) => table,
+    );
   }
 
   getModels(): ModelClass<Model>[] {
-    return Array.from(this.tableMap.values()).map(({ model }) => model);
+    return Array.from(this.models.getTableMap().values()).map(
+      ({ model }) => model,
+    );
   }
 }
---
+++ src/library/model.manager.ts
@@ -0,0 +1,154 @@
+import { Model, ModelClass, Pojo } from 'objection';
+import { ModelNotFound } from '../exception/model-not-found.exception';
+import { ITable } from '../types/table.interface';
+import { TableMap } from '../types/table.map';
+import { ColumnTools } from '../utils/column-tools';
+import type { Migrator } from './migrator';
+import { ValidateTable } from './table.validator';
+
+export class ModelManager {
+  protected tables: TableMap = new Map();
+
+  constructor(protected readonly migrator: Migrator) {}
+
+  public getTableMap(): TableMap {
+    return this.tables;
+  }
+
+  public setTables(tables: ITable[]): void {
+    this.tables.clear();
+
+    tables.forEach(table => this.addTable(table));
+  }
+
+  public validateRelations(): void {
+    for (const { table } of this.tables.values()) {
+      if (table.relations) {
+        for (const relationName in table.relations) {
+          if (
+            Object.prototype.hasOwnProperty.call(table.relations, relationName)
+          ) {
+            const relationDefinition = table.relations[relationName];
+
+            if (relationDefinition.references.table) {
+              if (
+                !this.migrator.reflection.isTableExists(
+                  relationDefinition.references.table,
+                )
+              ) {
+                throw new Error(
+                  `Table ${relationDefinition.references.table} not found`,
+                );
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  public addTable(table: ITable): void {
+    ValidateTable(table);
+
+    for (const columnName in table.columns) {
+      table.columns[columnName].capabilities.sort((a, b) => a - b);
+    }
+
+    const model = this.toModel(table);
+    this.tables.set(table.name, { table, model });
+  }
+
+  public removeTable(tableName: string): void {
+    this.tables.delete(tableName);
+  }
+
+  public getTable(tableName: string): ITable {
+    const table = this.tables.get(tableName);
+
+    if (!table) {
+      throw new Error(`Table ${tableName} not found`);
+    }
+
+    return table.table;
+  }
+
+  public getModel<T extends Model = Model>(tableName: string): ModelClass<T> {
+    const table = this.tables.get(tableName);
+
+    if (!table) {
+      throw new ModelNotFound(tableName);
+    }
+
+    return table.model as ModelClass<T>;
+  }
+
+  /**
+   * Convert the table into a model class.
+   */
+  protected toModel(table: ITable): ModelClass<Model> {
+    // Prepare fast lookup maps for both propery and column name conversions.
+    // Even tho this is a small amount of data, it's worth it to avoid
+    // having to iterate over the properties of the model class.
+    const columnMap = new Map<string, string>();
+    const propertyMap = new Map<string, string>();
+
+    for (const columnName in table.columns) {
+      if (Object.prototype.hasOwnProperty.call(table.columns, columnName)) {
+        // TODO map from meta defintion
+
+        // Map column names to property names
+        columnMap.set(columnName, columnName);
+        // Map property names to column names
+        propertyMap.set(columnName, columnName);
+      }
+    }
+
+    // Map database columns to code level references
+    // Database -> Model = Getter
+    const databaseToModel = (dbPojo: Pojo): Pojo => {
+      const modelPojo: Pojo = {};
+
+      for (const columnName of Object.keys(dbPojo)) {
+        modelPojo[columnMap.get(columnName)!] = dbPojo[columnName];
+      }
+
+      return dbPojo;
+    };
+
+    // Map code level references to database columns
+    // Database -> Model = Setter
+    const modelToDatabase = (modelPojo: Pojo): Pojo => {
+      const dbPojo: Pojo = {};
+
+      for (const propertyName of Object.keys(modelPojo)) {
+        dbPojo[propertyMap.get(propertyName)!] = modelPojo[propertyName];
+      }
+
+      return dbPojo;
+    };
+
+    // Hook before the model is created
+    const onCreate = () => {};
+
+    // Hook before the model is updated
+    const onUpdate = () => {};
+
+    const model = class extends Model {};
+
+    model.tableName = table.name;
+    model.idColumn = ColumnTools.filterPrimary(table);
+
+    model.columnNameMappers = {
+      parse: databaseToModel,
+      format: modelToDatabase,
+    };
+
+    // model.prototype.$beforeInsert = onCreate;
+    // model.prototype.$beforeUpdate = onUpdate;
+
+    // Associate the knex instance with the newly created model class.
+    model.knex(this.migrator.knex);
+
+    return model;
+  }
+}
